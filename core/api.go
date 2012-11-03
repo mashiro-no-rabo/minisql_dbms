f8354a8de73655e1823d45624d870862cecd362b
AquarHEAD <aquarhead@gmail.com>
2012-11-04 00:37:51 +0800

    define some api

---
+++ core/api.go
@@ -0,0 +1,42 @@
+package core
+
+import (
+	"fmt"
+)
+
+type Comparable interface {
+	Equals() bool
+	Less() bool
+	Greater() bool
+}
+
+type ColumnType interface {
+}
+
+func CreateTable() {
+
+}
+
+func CreateIndex() {
+
+}
+
+func DropTable() {
+
+}
+
+func DropIndex() {
+
+}
+
+func Insert() {
+
+}
+
+func Select() {
+
+}
+
+func Delete() {
+
+}
