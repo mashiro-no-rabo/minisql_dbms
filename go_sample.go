c3b64bee9218e3391c632148265c32478ba9a023
Pengyu CHEN <cpy.prefers.you@gmail.com>
2012-11-03 23:12:37 +0800

    sample added to show how to interact between C and Go using gccgo

---
+++ go_sample.go
@@ -0,0 +1,13 @@
+/* 
+ * Sample source for course work of Database System Design, 2012.09-11, Zhejiang University.
+ * Source code by Pengyu CHEN(cpy.prefers.you@gmail.com).
+ * COPYLEFT, ALL WRONGS RESERVED.
+ */
+
+package sample
+
+func GoFuncSample() {
+    print("This is message from a Go function.\n");
+    return
+}
+
