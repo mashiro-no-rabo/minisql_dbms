84e627cf4559e0f851f782e7018c8c160c980a35
lightxu <lightxuzju@gmail.com>
2012-11-09 16:17:08 +0800

    Update src/idxman/index_manager.go
    Basic modules are completed without debugged.

--- src/idxman/index_manager.go
+++ src/idxman/index_manager.go
@@ -1,25 +1,34 @@
 package idxman
 
-import (
-        "../common"
-)
-
+// CONST
 const order = 4 // We are implementing B+ Tree for n = 4.
 
+// INTERFACE
+type ValueType interface {
+    EqualsTo(ValueType) bool
+    LessThan(ValueType) bool
+}
+
+type KeyType interface {
+    GetValue() ValueType
+}
+
+// STRUCT
 type node struct {
-    isLeaf bool
     parent *node
     keyCnt int
     keys [] KeyType
-    // In leaf node, children[keyCnt] points to next leaf
+    // if it is a leaf, children[0] should point to its right sibling
     children [] *node
+    leaf bool
 }
 
 type idxMan struct {
     root *node
 }
 
-// func: NewIdxMan
+// PUBLIC FUNCTION
+
 // Creat an index manager and give it a empty root.
 func NewIdxMan() *idxMan {
     im := new(idxMan)
@@ -27,74 +36,128 @@ func NewIdxMan() *idxMan {
     return im
 }
 
+// Find the first KeyType containing given v,
+// return (nil, false) if nothing is found.
 func (self idxMan) Find(v ValueType) (KeyType, bool) {
-    return self.root.search(v)
+    l := self.root.findLeafNode(v)
+    i, found := l.findValueIndex(v)
+    temp := l.keys[i]
+    if found && temp.GetValue().EqualsTo(v) {
+        return temp, true
+    }
+    return nil, false
 }
 
+// Insert k into B+ Tree
 func (self *idxMan) Insert(k KeyType) {
-    l := self.root.findLeafNode(k.GetValue())
-    insertInLeaf(l, k)
+    l := self.root.findLeafNode(k.GetValue())    
+    l.insertKey(k)
+    // If the l is full, split it.
+    // Then insert two nodes l and l1 into their parent.
+    // Update root if new root is created.
     if l.isFull() {
-        l1, k1 := splitNode(l)
-        r, newRoot := insertInParent(l, k1, l1)
+        l1 := l.splitNode()
+        r, newRoot := l.insertInParent(l1)
         if (newRoot) {
             self.root = r
         }
     }
 }
 
-func (self *idxMan) Delete(k KeyType) bool {
-    l := self.root.findLeafNode(k.GetValue())
-    i, found := l.findLeafNode(k.GetValue())
+// Delete the first KeyType containing given v,
+// return (false) if nothing is deleted
+func (self *idxMan) Delete(v ValueType) (KeyType, bool) {
+    // Find the corresponding leaf l
+    l := self.root.findLeafNode(v)
+    
+    // Find the index of v in l
+    i, found := l.findValueIndex(v)
     if ! found {
-        return false
+        return nil, false
     }
-    root, newRoot := deleteEntry(l, i)
-    if (newRoot) {
-        self.root = root
+    k, _ := l.deleteKey(i)
+    
+    // Check node's children number back to root
+    for n := l; n.keyCnt < order / 2; n = n.parent {
+        if n.isRoot() && n.keyCnt == 1 {
+            self.root = n.children[0]
+            break
+        }
+        p := n.parent
+        i, _ := p.findValueIndex(n.minKey().GetValue())
+        if i == p.keyCnt - 1 {
+            n1 := p.children[i - 1]
+            if (n.keyCnt + n1.keyCnt < order) {
+                // Case0: merge n with its left brother
+                n1.mergeRightNode(n, p.keys[i - 1])
+                p.deleteChild(i)
+            } else {
+                // Case1: borrow a child from left brother
+                if n.isLeaf() {
+                    k, _ := n1.deleteKey(n1.keyCnt - 1)
+                    n.insertKey(k)
+                } else {
+                    c, _ := n1.deleteChild(n1.keyCnt - 1)
+                    n.insertChild(c)
+                }
+            }
+        } else {
+            n1 := p.children[i + 1]
+            if (n.keyCnt + n1.keyCnt < order) {
+                // Case2: merge n with its right brother
+                n.mergeRightNode(n1, p.keys[i])
+                p.deleteChild(i + 1)
+            } else {
+                // Case3: borrow a child from right brother
+                if n.isLeaf() {
+                    k, _ := n1.deleteKey(0)
+                    n.insertKey(k)
+                } else {
+                    c, _ := n1.deleteChild(0)
+                    n.insertChild(c)
+                }
+            }
+        }
     }
-    return true
+    
+    return k, true
 }
 
-// func: createNode
 // create an empty node and return its address
 func createNode() *node {
     x := new(node)
     x.keys = make([] KeyType, 0, order)
-    x.children = make([] KeyType, 0, order)
+    x.children = make([] *node, 0, order)
     return x
 }
 
-func (self node) findValueIndex(v ValueType) (i int, found bool) {
-    for i = 0; i < self.keyCnt; i++ {
-        temp := self.keys[i]
-        if ! temp.GetValue().LessThan(v) {
-            return i, true
-        }
+// PRIVATE FUNCTION
+
+// Split node and return the new node
+func (self *node) splitNode () *node {
+    // Create node
+    // The new node is the right brother of self
+    n := createNode()
+    n.leaf = self.isLeaf()
+    n.parent = self.parent
+    n.keyCnt = order / 2 + order % 2
+    if self.isLeaf() {
+        n.keys = append(n.keys, self.keys[order / 2 : ]...)
+        n.children[0] = self.children[0]
+        self.children[0] = n
+    } else {
+        n.keys = append(n.keys, self.keys[order / 2 : ]...)
+        n.children = append(n.children, self.children[order / 2 : ]...)
     }
-    return self.keyCnt, false
+    // Update self
+    self.keyCnt = order / 2
+    return n
 }
 
-func (self node) search(v ValueType) (KeyType, bool) {
-    x := self
-    for ! x.isLeaf {
-        i, found := x.findValueIndex(v)
-        temp := x.keys[i]
-        if found && temp.GetValue().IsEqual(v) {
-            return temp, true // Here we only return the first accepted key.
-        }
-        x = *x.children[i]
-    }
-    return nil, false
-}
-
-func (self node) findLeafNode(v ValueType) (l *node) {
-    x := self
-    for ! x.isLeaf {
-        i, _ := x.findValueIndex(v)
-        x = *x.children[i]
-    }
-    return &x
+func (self *node) mergeRightNode(rb *node, k KeyType) {
+    self.keys = append(self.keys, append([]KeyType{k}, rb.keys...)...)
+    self.children = append(self.children, rb.children...)
+    self.keyCnt += rb.keyCnt
 }
 
 func (self node) isFull() bool {
@@ -105,113 +168,120 @@ func (self node) isRoot() bool {
     return self.parent == nil
 }
 
-func (self *node) deleteChild(i int) (k KeyType, c *node) {
-    c = self.children[0]
-    if (i == 0) {
-        if self.isLeaf() {
-           k = nil 
-        } else {
-            k = self.children[0].keys[0]
-        }
-    } else {
-        k = self.keys[i - 1]
-    }
-    self.keys = append(self.keys[:i], self.keys[i + 1:]...)
-    self.children = append(self.children[:i + 1], self.children[i + 2:]...)
-    self.keyCnt--
-    return k, c
+func (self node) isLeaf() bool {
+    return self.leaf
 }
 
-func (self *node) insertChild(i int, k KeyType, c *node) {
-    if (i == 0） {
-        k1 = self.children[0].keys[0]
-        self.keys = append([]KeyType{k1}, self.keys...)
-        self.children = append([]*node{c}, self.children...}
+// func: minKey
+func (self node) minKey() KeyType {
+    var n *node
+    for n = &self; ! n.isLeaf(); n = n.children[0] {}
+    return n.keys[0]
+}
+
+// return first index of Child having key greater or
+// equal than v. 0 <= i <= keyCnt - 1
+// In a leaf, the ith Child is ith Key
+// In a non-leaf, the ith Child's key is (i - 1)th Key,
+// and 0th Child has no key in the Keys.
+func (self node) findValueIndex(v ValueType) (i int, found bool) {
+    for i = 0; i < self.keyCnt; i++ {
+        if ! self.keys[i].GetValue().LessThan(v) {
+            return i, true
+        }
+    }
+    return self.keyCnt, false
+}
+
+// Find the leaf node contains (or should contain) v.
+func (self node) findLeafNode(v ValueType) *node {
+    x := &self
+    for ! x.isLeaf() {
+        i, _ := x.findValueIndex(v)
+        x = x.children[i]
+    }
+    return x
+}
+
+// Insert k into leaf
+func (self *node) insertKey(k KeyType) bool {
+    // l should be a leaf and not full
+    if (! self.isLeaf()) || self.isFull() {
+        return false
+    }
+    
+    i, _ := self.findValueIndex(k.GetValue())
+    self.keys = append(self.keys[:i], append([]KeyType{k}, self.keys[i:]...)...)
+    self.keyCnt++
+    return true
+}
+
+// Insert c into non-leaf
+func (self *node) insertChild(c *node) bool {
+    // l should be a non-leaf and not full
+    if self.isLeaf() || self.isFull() {
+        return false
+    }
+    
+    c.parent = self
+    
+    k := c.minKey()
+    i, _ := self.findValueIndex(k.GetValue())
+    if (i == 0) {
+        k = self.minKey()
+        self.keys = append([]KeyType{k}, self.keys...)
+        self.children = append([]*node{c}, self.children...)
     } else {
-        self.keys = append(self.keys[:i], append([]KeyType{k}, self.keys[i:]...)...)
-        self.children = append(self.children[:i - 1], append([]*node{c}, self.children[i - 1:]...)...)
+        self.keys = append(self.keys[:i - 1], append([]KeyType{k}, self.keys[i - 1:]...)...)
+        self.children = append(self.children[:i], append([]*node{c}, self.children[i:]...)...)
     }
     self.keyCnt++
+    return true
 }
 
-func insertInLeaf(l *node, k KeyType) {
-    i, _ := l.findValueIndex(k.GetValue())
-    l.keys = append(l.keys[:i + 1], append([]KeyType{k}, l,keys[i + 1:])...)
-    l.keyCnt++
-}
-
-func splitNode(l *node) (l1 *node, k1 KeyType) {
-    // Create node l1
-    l1 = createNode()
-    l1.isLeaf = true
-    l1.parent = l.parent
-    l1.keyCnt = order / 2 + order % 2
-    // Copy keys from l to l1
-    copy(l1.keys, l.keys[0:order / 2])
-    // Update l
-    l.keyCnt = order / 2
-    l1.children[l1.keyCnt] = l.children[l.keyCnt]
-    l.children[l.keyCnt] = l1 
-    // return l1 and k1
-    return l1, l1.keys[0]
-}
-
-func insertInParent(l *node, k1 KeyType, l1 *node) (r *node, newRoot bool) {
-    // l is the root of the tree
-    if l.isRoot() {
+// Insert c into self's parent
+// If new root is created, it will return (root, newRoot).
+// Otherwise return (nil, false)
+// @_@ It is not functional programming, keep it stupid.
+func (self *node) insertInParent(c *node) (*node, bool) {
+    // If l is the root of the tree, split it and create new root.
+    if self.isRoot() {
         temp := new(node)
-        temp.keyCnt = 1
-        temp.keys[0] = k1
-        temp.children[0] = l
-        temp.children[1] = l1
+        temp.insertChild(self)
+        temp.insertChild(c)
         return temp, true
     }
     
-    p := l.parent
-    p.keys[p.keyCnt] = k1
-    p.children[p.keyCnt + 1] = l1 // Insert child link into right of key
-    p.keyCnt++
+    p := self.parent
+    p.insertChild(c)
     if p.isFull() {
-        p1, k2 := splitNode(p)
-        return insertInParent(p, k2, p1)
+        p1 := p.splitNode()
+        return p.insertInParent(p1)
     }
     return nil, false
 }
 
-func deleteEntry(n *node, i int) （root int, newRoot bool） {
-    // Delete k from l
-    n.keys = append(n.keys[:i], n.keys[i + 1:])
-    n.children = append(n.children[:i + 1], n.children[i + 2:])
-    n.keyCnt--
-    // if n is the root and n has only one remaining children
-    if n.isRoot() && n.keyCnt == 1 {
-        return n.children[0], true
+// Delete ith key.
+func (self *node) deleteKey(i int) (KeyType, bool) {
+    if ! self.isLeaf() {
+        return nil, false
     }
-    // if n has too few values
-    if n.keyCnt < order / 2 {
-        p := n.parent
-        i1 := p.findValueIndex(n.keys[0].GetValue())
-        if i == p.keyCnt - 1 {
-            n1 := p.children[i1 - 1]
-            if (n.keyCnt + n1.keyCnt < order) {
-                p.children[i1 - 1] = merge(n1, n)
-            } else {
-                // borrow entry from i1 - 1
-               k, c := n1.delete(n1.keyCnt)
-               n.insertChild(0, k, c)
-            }
-            deleteEntry(p, i1)
-        } else {
-            n1 := p.children[i1 + 1]
-            if (n.keyCnt + n1,keyCnt < order) {
-                p.children[i] = merge(n, n1)
-            } else {
-                // borrow an entry from i1 + 1
-                k, c := n1.delete(0)
-                n.insertChild(n.keyCnt, k, c)
-            }
-            deleteEntry(p, i1 + 1)
-        }
-        
+    k := self.keys[i]
+    self.keys = append(self.keys[ :i], self.keys[i + 1: ]...)
+    return k, true
+}
+
+// Delete ith child, return its minimum key and pointer
+func (self *node) deleteChild(i int) (*node, bool) {
+    if self.isLeaf() {
+        return nil, false
     }
+    
+    c := self.children[i]
+    
+    self.keys = append(self.keys[:i - 1], self.keys[i:]...)
+    self.children = append(self.children[:i], self.children[i + 1:]...)
+    self.keyCnt--
+    
+    return c, true
 }
